7b9f7e9c11ac1c018e30d70db10c7bd87252e10b
BarBQFoot <anusorn.lt2006@gmail.com>
2026-02-23 00:27:22 +0700

    Add files via upload

--- routes/my_events.php
+++ routes/my_events.php
@@ -1,11 +1,23 @@
 <?php
 
-$user_id = 1;
-$status = $_GET['status'] ?? '';
 
-$result = getMyEvents($user_id, $status);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_POST['cancel'])) {
+
+        $event_id = $_POST['event_id'];
+        $user_id = $_SESSION['user_id'];   // หรือ $_SESSION['uid']
+
+        cancelEvent($user_id, $event_id);
+
+        header("Location: /my_events");
+        exit();
+    }
+}
+
+$status = $_GET['status'] ?? '';
+$result = getMyEvents($_SESSION['user_id'], $status);
 
 renderView('my_events', [
-    'title' => 'กิจกรรมของฉัน',
     'result' => $result
 ]);
