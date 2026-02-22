c99f581aae2f21c5747a49e7ea771dc913d224b9
BarBQFoot <anusorn.lt2006@gmail.com>
2026-02-22 18:27:33 +0700

    Add files via upload

--- routes/home.php
+++ routes/home.php
@@ -1,4 +1,28 @@
 <?php
 // ประมวลผลก่อนแสดงผลหน้า
 unset($_SESSION['eid']);
-renderView('home', ['title' => 'Welcome to Home Page']);
+
+$keyword = $_GET['keyword'] ?? '';
+$start   = $_GET['start'] ?? '';
+$end     = $_GET['end'] ?? '';
+
+if ($keyword != '' || ($start != '' && $end != '')) {
+    $result = searchEvents($keyword, $start, $end);
+} else {
+    $result = getNotinEvets(1);   
+}
+
+renderView('home', [
+    'title' => 'Welcome to Home Page',
+    'result' => $result
+]);
+if (isset($_POST['join'])) {
+
+    $event_id = $_POST['event_id'];
+    $user_id = 1;
+
+    joinEvent($user_id, $event_id);
+
+    header("Location: /home");
+    exit();
+}
