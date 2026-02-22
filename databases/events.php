ec86752378c77c78733cfe8a8e59fbbd6c05ec07
fluketerr <flukeyssp@gmail.com>
2026-02-22 19:56:30 +0700

    merge code four fluke man

--- databases/events.php
+++ databases/events.php
@@ -67,3 +67,74 @@ function deleteEventById(int $id, $conn): bool
     $stmt->execute();
     return $stmt->affected_rows > 0;
 }
+function searchEvents($keyword, $start, $end)
+{
+    global $conn;
+
+    $sql = "SELECT * FROM Events WHERE 1";
+
+    if ($keyword != '') {
+        $keyword = strtolower($keyword);
+        $sql .= " AND LOWER(event_name) LIKE '%$keyword%'";
+    }
+
+    if ($start != '' && $end != '') {
+        $sql .= " AND start_date >= '$start'
+                  AND end_date <= '$end'";
+    }
+
+    return $conn->query($sql);
+}
+function joinEvent($user_id, $event_id)
+{
+    global $conn;
+
+    $sql = "INSERT INTO Registrations (uid, eid, status)
+            VALUES ('$user_id', '$event_id', 'wait')";
+
+    return $conn->query($sql);
+}
+function getMyEvents($user_id, $status = '')
+{
+    global $conn;
+
+    $sql = "SELECT e.*, r.status
+            FROM Registrations r
+            JOIN Events e ON r.eid = e.eid
+            WHERE r.uid = '$user_id'";
+
+     if ($status != '') {
+        $sql .= " AND r.status = '$status'";
+    }
+
+    return $conn->query($sql);
+}
+
+function updateEvent(array $event, mysqli $conn): bool
+{
+    $sql = "UPDATE Events 
+            SET event_name = ?, 
+                event_detail = ?, 
+                start_date = ?, 
+                end_date = ?, 
+                event_capacity = ?, 
+                event_status = ?
+            WHERE eid = ?";
+
+    $stmt = $conn->prepare($sql);
+
+    $stmt->bind_param(
+        'ssssisi',
+        $event['name'],
+        $event['detail'],
+        $event['start'],
+        $event['end'],
+        $event['capacity'],
+        $event['status'],
+        $event['eid']
+    );
+
+    $stmt->execute();
+
+    return $stmt->affected_rows >= 0;
+}
