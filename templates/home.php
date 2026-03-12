98381273320bbbb7330eedc270fde475704ab9f0
IIrumix <callcenter.cc8gmai.com>
2026-03-12 21:58:36 +0700

    button position

--- templates/home.php
+++ templates/home.php
@@ -135,14 +135,14 @@
                             unset($_SESSION['message']); ?></p>-->
                 </div>
                 <?php if (!isset($_SESSION['user_id'])) { ?>
-                    <a class="fixed bottom-5 right-5 w-14 h-14 py-6 z-50 bg-[#DBC3D6]  rounded-full flex items-center justify-center text-4xl font-semibold text-black shadow-sm hover:bg-[#DDAED3] hover:text-white transition-colors"
+                    <a class="fixed bottom-10 right-10 w-14 h-14 py-6 z-50 bg-[#DBC3D6]  rounded-full flex items-center justify-center text-4xl font-semibold text-black shadow-sm hover:bg-[#DDAED3] hover:text-white transition-colors"
                         href="/login">
                         <svg fill="white" width="30" height="30" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
                             <path d="M17,11H13V7a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1v4H7a1,1,0,0,0-1,1H6a1,1,0,0,0,1,1h4v4a1,1,0,0,0,1,1h0a1,1,0,0,0,1-1V13h4a1,1,0,0,0,1-1h0A1,1,0,0,0,17,11Z" />
                         </svg>
                     </a>
                 <?php } else { ?>
-                    <a class="fixed bottom-5 right-5 w-14 h-14 py-6 z-50 bg-[#DBC3D6]  rounded-full flex items-center justify-center text-4xl font-semibold text-black shadow-sm hover:bg-[#DDAED3] hover:text-white transition-colors"
+                    <a class="fixed bottom-10 right-10 w-14 h-14 py-6 z-50 bg-[#DBC3D6]  rounded-full flex items-center justify-center text-4xl font-semibold text-black shadow-sm hover:bg-[#DDAED3] hover:text-white transition-colors"
                         href="/event_create">
                         <svg fill="white" width="30" height="30" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512">
                             <path d="M17,11H13V7a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1v4H7a1,1,0,0,0-1,1H6a1,1,0,0,0,1,1h4v4a1,1,0,0,0,1,1h0a1,1,0,0,0,1-1V13h4a1,1,0,0,0,1-1h0A1,1,0,0,0,17,11Z" />
