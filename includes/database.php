94f4ed4c44f88de0a6b53a94cf780c8ac73d1de5
BarBQFoot <anusorn.lt2006@gmail.com>
2026-03-15 03:46:54 +0700

    -

--- includes/database.php
+++ includes/database.php
@@ -21,7 +21,7 @@ function getConnection(): mysqli
 }
 
 // database functions ต่างๆ
-require_once DATABASES_DIR . '/user.php';
+
 require_once DATABASES_DIR . '/events.php';
 require_once DATABASES_DIR . '/pictures.php';
 require_once DATABASES_DIR . '/registration.php';
